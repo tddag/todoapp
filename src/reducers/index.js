633530da316899c4839e241eeba34bb778744b01
Tam Dang <tamdang@tams-imac.lan>
2021-09-07 18:21:09 -0400

    CSS styling

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ const mockTasks = [
       id: 1, 
       title: 'Learn Redux',
       description: 'The store, actions, and reducers, oh my!',
-      status: 'In Progress',
+      status: 'Unstarted',
     },
     {
       id: 2,
@@ -11,6 +11,18 @@ const mockTasks = [
       description: 'No big deal.',
       status: 'In Progress',
     },
+    {
+        id: 3,
+        title: 'Go to gym',
+        description: 'Deadlift, Squat, Bench Press',
+        status: 'Completed',
+      },
+      {
+        id: 4,
+        title: 'Learn guitar',
+        description: 'Anatomy, Holding a guitar, Tuning a guitar, playing open chords, strumming in rhythm',
+        status: 'In Progress',
+      },
   ];
 
 export default function tasks(state = {tasks: mockTasks}, action) {
