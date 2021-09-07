aae765d1de06d8085eb241244ab4b927b30a6aea
Tam Dang <tamdang@tams-imac.lan>
2021-09-07 16:40:36 -0400

    configured Redux Store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import tasks from './reducers';
+
+
+const store = createStore(tasks);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
 
